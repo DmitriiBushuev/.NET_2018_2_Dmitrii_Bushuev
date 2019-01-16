50587dfe42e08f61a512d43f6488cd794ef12ec7
Dmitrii Bushuev <dmitrii.bushuev93@mail.ru>
2019-01-16 12:35:42 +0300

    Snake - курс Geekbrains lesson7

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -18,32 +18,13 @@ namespace Snake
             p2.Draw();
 
             Point p3 = new Point(7, 2,'&');
+            p3.Draw();
 
-            List<int> numList = new List<int>();
-            numList.Add(0);
-            numList.Add(1);
-            numList.Add(2);
+            HorizontalLine hLine1 = new HorizontalLine(5, 10, 8, '+');
+            hLine1.Draw();
 
-            int x = numList[0];
-            int y = numList[1];
-            int z = numList[2];
-
-            foreach (int i in numList)
-            {
-                Console.WriteLine(i);
-            }
-
-            numList.Remove(0);
-
-            List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            pList.Add(p2);
-            pList.Add(p3);
-
-            List<char> charList = new List<char>();
-            charList.Add('%');
-            charList.Add('@');
-            charList.Add('!');
+            VerticalLine vLine1 = new VerticalLine(12, 2, 10, '&');
+            vLine1.Draw();
 
 
             Console.ReadLine();
